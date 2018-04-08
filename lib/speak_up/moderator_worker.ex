ef01bb13078b7d0ecf24106dc4183225b20b242c
Krishna Kumar <krishnak@thoughtworks.com>
2018-04-08 22:17:39 +0530

    [Krishna] supporting mute and canspeak endpoints

--- lib/speak_up/moderator_worker.ex
+++ lib/speak_up/moderator_worker.ex
@@ -11,17 +11,28 @@ defmodule SpeakUp.ModeratorWorker do
 
   def handle_call(:get_all, _from, state) do
     participants = GenServer.call({:global, :moderator}, :registered)
-    ids = Enum.map(participants, &(elem(&1, 2)))
+    ids = Enum.map(participants, &({elem(&1, 2),elem(&1, 3)}))
+    IO.inspect(ids)
     {:reply, ids, state}
   end
 
   def handle_call({:mute,id}, _from, state) do
-    reply = GenServer.call({:global, :moderator}, {:mute, id})
-    {:reply, reply, state}
+    case  GenServer.call({:global, :moderator}, {:mute, id}) do
+       :ok ->
+         {:reply, :ok, state}
+         _ ->
+          IO.puts("Cannot mute")
+          {:reply, :error, state}
+        end
   end
 
   def handle_call({:can_speak, id}, _from, state) do
-    reply = GenServer.call({:global, :moderator}, {:can_speak, id})
-    {:reply, reply, state}
+    case GenServer.call({:global, :moderator}, {:can_speak, id}) do
+       :ok ->
+         {:reply, :ok , state}
+         _ ->
+           IO.puts("Cannot speak")
+           {:reply, :error, state}
+        end
   end
 end
