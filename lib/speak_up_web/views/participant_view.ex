8ab364f1cbaa203271239f6c313fbbd67640c3b9
Krishna Kumar <krishnak@thoughtworks.com>
2018-05-01 16:53:23 +0530

    [Krishna] Added some styling for names on pages

--- lib/speak_up_web/views/participant_view.ex
+++ lib/speak_up_web/views/participant_view.ex
@@ -10,4 +10,14 @@ defmodule SpeakUpWeb.ParticipantView do
         false
     end
   end
+
+  def getParticipantNameFromCookie(conn) do
+    cookie = conn.cookies["participant_cookie"]
+    case Phoenix.Token.verify(SpeakUpWeb.Endpoint, "H|$|<>V0|@-||E$@|-_", cookie, max_age: 86400) do
+      {:ok, participantName} ->
+        {true, participantName}
+      {:error, e} ->
+        {false, "Unknown"}
+    end
+  end
 end
